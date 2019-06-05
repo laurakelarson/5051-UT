48df9a3b4d6dfe715bb17c4d233e92a01ef7c2dd
Laura Larson <larsonlaura@seattleu.edu>
2019-06-04 22:30:02 -0700

    Added LogViewModel_Get_Default_Should_Pass to ReportViewModel test methods

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -44,5 +44,32 @@ namespace UnitTests.Models
             // Assert
             Assert.AreEqual(result, 1);
         }
+
+        [TestMethod]
+        public void ReportViewModel_LogViewModel_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new ReportViewModel();
+
+            // Act
+            var result = myTest.LogViewModel;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        /*[TestMethod]
+        public void ReportViewModel_NumberOfUsers_Set_Default_Should_Pass()
+        {
+            // Arrange
+            var myTest = new ReportViewModel();
+
+            // Act
+            myTest.NumberOfUsers = 1;
+            var result = myTest.NumberOfUsers;
+
+            // Assert
+            Assert.AreEqual(result, 1);
+        }*/
     }
 }
