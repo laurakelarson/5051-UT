6c6110f2f46e5dc067b4075895cfc3f8605f71f9
Laura Larson <larsonlaura@seattleu.edu>
2019-06-04 21:51:31 -0700

    Added LogViewModelTests.cs to the project

---
+++ UnitTests/Models/LogViewModelTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HW1c.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class LogViewModelTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
